56fc739fc205bd476321353b8426c7ae3125664e
mika-yan <kryuger.mikaelyan@inbox.ru>
2020-06-20 23:38:05 +0300

    Updates

---
+++ precache-manifest.c59e7e0013a0c8b9f08ccfe00ad57d0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc45d1622cb3d694e45c888d10e1501",
+    "url": "/getpassword/index.html"
+  },
+  {
+    "revision": "fb88fdbcb9d27fa9bb03",
+    "url": "/getpassword/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "36e3aff69bd0b52acd11",
+    "url": "/getpassword/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "fb88fdbcb9d27fa9bb03",
+    "url": "/getpassword/static/js/2.5049d72f.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/getpassword/static/js/2.5049d72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e3aff69bd0b52acd11",
+    "url": "/getpassword/static/js/main.5189453d.chunk.js"
+  },
+  {
+    "revision": "3686149f8cc050c5d8a9",
+    "url": "/getpassword/static/js/runtime-main.ef606ca3.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/getpassword/static/media/persik.4e1ec840.png"
+  }
+]);
